dc6c4e944a557f261c1d88c729468c2baccf48bf
caron <etienne.caron@inist.fr>
2017-04-10 11:10:40 +0200

    Récupération de l'ID biblio ISTEX Google Scholar

--- src/options/options.js
+++ src/options/options.js
@@ -4,23 +4,35 @@
 // otherwise mode is 0 and nothing shall be closed automatically to avoid an ISTEX totalitarian behaviour
 var mode = 0;
 var optionsTabId;
+var istexLibraryId = "2733342842941555958"; //2733342842941555958;
 
 document.addEventListener('click', (e) => {
 
   if (e.target.classList.contains('accept')) {
     console.log('Accepter');
 
-    mode = 2;
+    //mode = 2;
 
-    chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
-      optionsTabId = tab[0].id;
-      chrome.tabs.create({
-        //url: "https://scholar.google.fr/scholar_setprefs?sciifh=1&inststart=0&num=10&scis=no&scisf=4&instq=istex&inst=3094930661629783031&context=istex&save=#2"
-        url: "https://scholar.google.fr/scholar_setprefs?instq=istex&inst=3094930661629783031&ctxt=istex&save=#2"
-      });
-      chrome.tabs.create({
-        //url: "https://scholar.google.fr/scholar_setprefs?sciifh=1&inststart=0&num=10&scis=no&scisf=4&instq=istex&inst=3094930661629783031&context=istex&save=#2"
-        url: "https://scholar.google.com/scholar_setprefs?instq=istex&inst=3094930661629783031&ctxt=istex&save=#2"
+    chrome.tabs.query({ currentWindow: true, active: true }, function(optionTab) {
+      optionsTabId = optionTab[0].id;
+
+      $.ajax({
+        url: "https://scholar.google.fr/scholar_setprefs?instq=istex",
+        dataType: 'html'
+      }).done(function(data) {
+        var parser = new DOMParser(),
+          doc = parser.parseFromString(data, "text/html");
+        istexLibraryId = doc.getElementsByName('inst')[0].value;
+        console.log(istexLibraryId);
+
+        chrome.tabs.create({
+          //url: "https://scholar.google.fr/scholar_setprefs?sciifh=1&inststart=0&num=10&scis=no&scisf=4&instq=istex&inst=3094930661629783031&context=istex&save=#2"
+          url: "https://scholar.google.fr/scholar_setprefs?instq=istex&inst=" + istexLibraryId + "&ctxt=istex&save=#2"
+        });
+        chrome.tabs.create({
+          //url: "https://scholar.google.fr/scholar_setprefs?sciifh=1&inststart=0&num=10&scis=no&scisf=4&instq=istex&inst=3094930661629783031&context=istex&save=#2"
+          url: "https://scholar.google.com/scholar_setprefs?instq=istex&inst=" + istexLibraryId + "&ctxt=istex&save=#2"
+        });
       });
     });
   };
