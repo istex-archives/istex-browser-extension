87dad3086d19e23e592705a608f6dc259c6a6b48
caron <etienne.caron@inist.fr>
2017-05-02 11:13:54 +0200

    Mise à jour de JQuery et version non minifiée

--- src/background/tabBootstrap.js
+++ src/background/tabBootstrap.js
@@ -15,8 +15,8 @@ chrome.runtime.onConnect.addListener(function(port) {
         || !isWhiteListed(port.sender.url)
     ) return;
 
-    chrome.tabs.executeScript(port.sender.tab.id, {file: '/vendors/jquery-3.1.1.min.js'});
-    chrome.tabs.executeScript(port.sender.tab.id, {file: '/vendors/lz-string.min.js'});
+    chrome.tabs.executeScript(port.sender.tab.id, {file: '/vendors/jquery-3.2.1.js'});
+    chrome.tabs.executeScript(port.sender.tab.id, {file: '/vendors/lz-string.js'});
     chrome.tabs.executeScript(port.sender.tab.id, {file: '/content_scripts/log.js'});
     chrome.tabs.executeScript(port.sender.tab.id, {file: '/content_scripts/storage.js'});
     chrome.tabs.executeScript(port.sender.tab.id, {file: '/content_scripts/main.js'});
